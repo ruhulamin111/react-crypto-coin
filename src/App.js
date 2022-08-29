63a1c9ba5e62aa54f38a38abe496b88f10d9616c
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-29 08:01:06 +0600

    header set up done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-
+import Home from './components/Home/Home';
+import Coins from './components/Coins/Coins';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
-        <Route></Route>
-
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/coins' element={<Coins></Coins>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
-
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+
 
 const Header = () => {
+    const style = ({ isActive }) => {
+        return {
+            color: isActive ? 'green' : '',
+            border: isActive ? '1px solid gray' : '',
+        }
+    }
+
     return (
-        <div>
+        <div className='w-11/12 mx-auto h-20'>
+            <nav className='flex justify-between items-center mt-4 '>
+                <div className='text-2xl font-bold' >
+                    <NavLink className='p-2 ' to='/'>Crypto Coins</NavLink>
+                </div>
+                <div className='text-xl'>
+                    <NavLink className='ml-10 p-2 rounded-lg' style={style} to='/home'>Home</NavLink>
+                    <NavLink className='ml-10 p-2 rounded-lg' style={style} to='/coins'>Coins</NavLink>
+                    <NavLink className='ml-10 p-2 rounded-lg' style={style} to='/about'>About</NavLink>
+                </div>
+            </nav>
 
         </div>
     );
