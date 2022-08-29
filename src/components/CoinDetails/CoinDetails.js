b01c8a6a536907f73555562d97164e77dafdd18e
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-29 10:31:01 +0600

    single coin details complete

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -4,15 +4,24 @@ import useCoin from '../hooks/useCoin/useCoin';
 
 const CoinDetails = () => {
     const { id } = useParams();
-    const [coin, setCoin] = useCoin(id);
+    const [coin] = useCoin(id);
 
     return (
-        <div>
-            {coin.name}
-            <div>
-
+        <div className='w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 h-full justify-items-center content-center' >
+            <div className=''>
+                <p className='text-2xl font-bold my-3'>Genereal Information :</p>
+                <p><b>Name : </b>{coin.name}</p>
+                <p><b>Origin : </b>{coin.country_origin ? coin.country_origin : 'Not available'}</p>
+                <p><b>Cetogories : </b>{coin.categories?.[0] ? coin.categories?.[0] : 'Not available'}</p>
+                <p className='text-2xl font-bold my-3'>Score Information :</p>
+                <p><b>Coin Rank : </b>{coin.coingecko_rank}</p>
+                <p><b>Coin Score : </b>{coin.coingecko_score}</p>
+                <p><b>Community Score : </b>{coin.community_score}</p>
+                <p><b>Description : </b>{coin.description?.en.slice(0, 500) ? coin.description?.en.slice(0, 500) : 'Not available'}</p>
             </div>
-
+            <div className='flex justify-center items-center'>
+                <img src={coin.image?.large} alt="coin" />
+            </div>
         </div>
     );
 };
--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -8,7 +8,7 @@ const Coins = () => {
     return (
         <div className='w-11/12 mx-auto'>
             <p className='text-3xl font-bold text-center my-12'>All Crypto Currency Coins</p>
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 my-10'>
                 {
                     coins.map((coin, index) => <Coin
                         key={index}
