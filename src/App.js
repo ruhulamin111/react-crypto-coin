5215b0f91691e33269e666de468a4f5c37edf385
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-29 07:18:10 +0600

    install tailwind and router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route></Route>
+
+      </Routes>
 
     </div>
   );
